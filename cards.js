68c2df3d5726ba33e6ae6a6cbd5e94f7c63d2d85
JADACARD <jadacard23@gmail.com>
2026-06-21 20:37:34 +0300

    Delete cards.js

--- cards.js
+++
@@ -1,4 +0,0 @@
-export const CARDS = [
-  { id: 'black_onyx', name: 'Black Onyx', price: 5, daily: 2, ref: 5, level: 1, img: 'https://i.ibb.co/5W0Qp7m/black-onyx.png' },
-  { id: 'blue_sapphire', name: 'Blue Sapphire', price: 10, daily: 5, ref: 10, level: 2, img: 'https://i.ibb.co/6sZ2bGg/blue-sapphire.png' },
-  { id: 'gold_bar', name: 'Gold Bar', price: 
